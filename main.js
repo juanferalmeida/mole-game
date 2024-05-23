be207d912d0115f209e586eda0f23638bae863a7
Juan Almeida <juan.almeida@sngular.com>
2024-05-23 22:26:11 +0200

    service worker

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ window.addEventListener('load', () => {
   initRouter(document.querySelector('#app'));
 });
 
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', function () {
-//     navigator.serviceWorker
-//       .register('/service-worker.js')
-//       .then(res => console.log('Service worker registered'))
-//       .catch(err => console.log('Service worker not registered', err));
-//   });
-// }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(res => console.log('Service worker registered'))
+      .catch(err => console.log('Service worker not registered', err));
+  });
+}
 
